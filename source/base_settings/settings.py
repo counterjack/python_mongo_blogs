b47ce589c513b27538105920ac1e1412475de090
Ankur Agrawal <ankurnitp@gmail.com>
2018-08-01 11:11:13 +0530

    added some constants and db settings in settings file

---
+++ source/base_settings/settings.py
@@ -0,0 +1,12 @@
+# will consists all the required settings used across the app
+
+DATABASE_SETTINGS = {
+	'db_name': 'blogs',
+	'username': '',
+	'password': '',
+	'port': 27017 # default mongod port
+	'host': ''
+}
+
+
+
---
+++ source/views_cluster/constants.py
@@ -0,0 +1,5 @@
+
+# will consists constants used across the app
+
+RESULTS_ON_A_PAGE = 10
+
